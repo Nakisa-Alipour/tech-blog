1063a461ac6e09eacc527dd413f55d660e646569
n.alipour777@yahoo.com <n.alipour777@yahoo.com>
2023-06-07 02:39:42 +1000

    added login, sign up and single post for home-routes.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -32,6 +32,61 @@ router.get('/', async (req, res) => {
   }
 });
 
+
+// Login route
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    // Render the login template
+    res.render('login');
+});
+  
+// Sign up route
+router.get('/signup', (req, res) => {
+    // Render the signup template
+    res.render('signup');
+});
+
+// Get single post route
+router.get('/post/:id', async (req, res) => {
+    try {
+        // Find the post with the provided ID, including its associated user and comments
+        const postData = await Post.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+                {
+                    model: Comment,
+                    include: {
+                        model: User,
+                        attributes: ['username'],
+                    },
+                },
+            ],
+        });
+        
+        if (!postData) {
+            // If no post is found with the provided ID, return a 404 error
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        
+        // Serialize the data
+        const post = postData.get({ plain: true });
+  
+        // Render the single post template with the post data
+        res.render('single-post', { post });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+    }
+});
+  
+  
 module.exports = router;
 
 
