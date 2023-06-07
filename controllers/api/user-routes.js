afc4e435ba932bce36602876dd75a023fd9cee7b
n.alipour777@yahoo.com <n.alipour777@yahoo.com>
2023-06-07 20:14:24 +1000

    added retrieve all user and user by id

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,69 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+// Get all users
+router.get('/', async (req, res) => {
+    try {
+        const userData = await User.findAll({
+            attributes: { exclude: ['password'] },
+            include: [
+                {
+                    model: Post,
+                    attributes: ['id', 'title', 'content', 'created_at'],
+                },
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'created_at'],
+                    include: {
+                        model: Post,
+                        attributes: ['title'],
+                    },
+                },
+            ],
+        });
+  
+        const users = userData.map((user) => user.get({ plain: true }));
+  
+        res.status(200).json(users);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Get a user by ID
+router.get('/:id', async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.params.id, {
+            attributes: { exclude: ['password'] },
+            include: [
+                {
+                    model: Post,
+                    attributes: ['id', 'title', 'content', 'created_at'],
+                },
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'created_at'],
+                    include: {
+                        model: Post,
+                        attributes: ['title'],
+                    },
+                },
+            ],
+        });
+  
+        if (!userData) {
+            res.status(404).json({ message: 'No user found with this id!' });
+            return;
+        }
+  
+        const user = userData.get({ plain: true });
+  
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 // Create a new user
 router.post('/', async (req, res) => {
     try {
@@ -23,7 +86,7 @@ router.post('/login', async (req, res) => {
         const user = await User.findOne({
             where: {
             email: req.body.email,
-        },
+            },
         });
 
         if (!user) {
